3fe141599fff6095ebf65962d2fbf80c06d3a000
Benoit <47849605+BPTdev@users.noreply.github.com>
2022-03-21 14:01:09 +0100

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
     <article>
         <h1><?= $post->title; ?></h1>
 
-        By <a href="#">{{$post->users->name}}</a> in <a
+        By <a href="#">{{$post->user->name}}</a> in <a
             href="categories/{{$post->category->slug}}">{{$post->category->name}}</a>
 
         <div><?= $post->body; ?></div>
